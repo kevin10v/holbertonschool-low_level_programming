409f41877fe7031e7996209504a9f82f56a7070f
kevin10v <vokakevin@icloud.com>
2025-06-09 14:05:56 +0000

    new files

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,7 +11,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
+
 	int n;
 
 	srand(time(0));
@@ -19,14 +21,19 @@ int main(void)
 
 	if (n > 0)
 	{
+
 		printf("%d is positive\n", n);
 	}
+
 	else if (n == 0)
 	{
+
 		printf("%d is zero\n", n);
 	}
+
 	else
 	{
+
 		printf("%d is negative\n", n);
 	}
 
