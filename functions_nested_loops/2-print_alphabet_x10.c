53f7982b90e164a3e9ef3fb9ef4b6fd8d5a9d884
kevin10v <vokakevin@icloud.com>
2025-06-11 08:18:31 +0000

    new files

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times,
+ * each followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
