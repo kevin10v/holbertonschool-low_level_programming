e9fdaa4f25785158325b7d629cd841999a464e84
kevin10v <vokakevin@icloud.com>
2025-06-19 09:54:07 +0000

    added file

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -6,18 +6,17 @@
  * @src: source string
  * Return: pointer to dest
  */
- char *_strcpy(char *dest, char *src)
- {
+char *_strcpy(char *dest, char *src)
+{
 	int i = 0;
 
-    
+
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	
+
 	dest[i] = '\0';
 	return (dest);
- }
- 
+}
