002f630e290bcc07f21e4b00acaa04cc6d4c1104
kevin10v <vokakevin@icloud.com>
2025-07-02 11:25:36 +0000

    update

---
+++ structures_typedef/README.md
@@ -0,0 +1,13 @@
+# C - Structures & typedef
+
+This project covers the basics of using structures and typedef in C:
+
+- Defining and using `struct`
+- Grouping different data types in one unit
+- Creating type aliases with `typedef`
+- Simplifying code readability and usage
+
+## Files
+- `struct_example.c`
+- `typedef_example.c`
+- `main.h`
---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new type struct dog
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: owner of the dog (string)
+ *
+ * Description: Struct to store basic info about a dog.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
+
