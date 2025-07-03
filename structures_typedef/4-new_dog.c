4f7d7a90074c15eca94d1a1b1ef5eb68f86095f6
kevin10v <vokakevin@icloud.com>
2025-07-03 09:09:11 +0000

    update

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,36 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+ * _strlen - calculates length of a string
+ * @s: string to measure
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+i++;
+return (i);
+}
+
+/**
+ * _strcpy - copies a string to a buffer
+ * @dest: destination buffer
+ * @src: source string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -23,8 +53,8 @@ new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
 
-name_len = strlen(name);
-owner_len = strlen(owner);
+name_len = _strlen(name);
+owner_len = _strlen(owner);
 
 name_copy = malloc(name_len + 1);
 if (name_copy == NULL)
@@ -32,7 +62,7 @@ if (name_copy == NULL)
 free(new_dog);
 return (NULL);
 }
-strcpy(name_copy, name);
+_strcpy(name_copy, name);
 
 owner_copy = malloc(owner_len + 1);
 if (owner_copy == NULL)
@@ -41,7 +71,7 @@ free(name_copy);
 free(new_dog);
 return (NULL);
 }
-strcpy(owner_copy, owner);
+_strcpy(owner_copy, owner);
 
 new_dog->name = name_copy;
 new_dog->age = age;
