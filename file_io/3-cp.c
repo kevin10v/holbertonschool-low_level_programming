0343124e83cd1b1d9ff28e881c4a12cd3e0cfbdf
kevin10v <vokakevin@icloud.com>
2025-07-24 10:43:30 +0000

    update

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,6 +7,11 @@
 
 #define BUFFER_SIZE 1024
 
+/**
+ * closefd - Closes two file descriptors.
+ * @fd1: First file descriptor.
+ * @fd2: Second file descriptor.
+ */
 void closefd(int fd1, int fd2)
 {
 	if (close(fd1) == -1)
@@ -21,10 +26,45 @@ void closefd(int fd1, int fd2)
 	}
 }
 
+/**
+ * copy_file - Copies content from one file to another.
+ * @fdr: File descriptor of the source file.
+ * @fdw: File descriptor of the destination file.
+ * @argv: Argument vector (used for error messages).
+ */
+void copy_file(int fdr, int fdw, char *argv[])
+{
+	char buffer[BUFFER_SIZE];
+	ssize_t n_read, n_written;
+
+	while ((n_read = read(fdr, buffer, BUFFER_SIZE)) > 0)
+	{
+		n_written = write(fdw, buffer, n_read);
+		if (n_written == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			closefd(fdr, fdw);
+			exit(99);
+		}
+	}
+	if (n_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		closefd(fdr, fdw);
+		exit(98);
+	}
+}
+
+/**
+ * main - Entry point for copying file content.
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Return: 0 on success, exits with code on error.
+ */
 int main(int argc, char *argv[])
 {
-	int fdr, fdw, n_read, n_written;
-	char buffer[BUFFER_SIZE];
+	int fdr, fdw;
 
 	if (argc != 3)
 	{
@@ -39,15 +79,6 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	/* ✅ try to read BEFORE opening output file */
-	n_read = read(fdr, buffer, BUFFER_SIZE);
-	if (n_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fdr);
-		exit(98);
-	}
-
 	fdw = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdw == -1)
 	{
@@ -56,24 +87,7 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	while (n_read > 0)
-	{
-		n_written = write(fdw, buffer, n_read);
-		if (n_written == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			closefd(fdr, fdw);
-			exit(99);
-		}
-		n_read = read(fdr, buffer, BUFFER_SIZE);
-		if (n_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			closefd(fdr, fdw);
-			exit(98);
-		}
-	}
-
+	copy_file(fdr, fdw, argv);
 	closefd(fdr, fdw);
 	return (0);
 }
