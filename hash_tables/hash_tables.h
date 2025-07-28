40e8e36fcae98252d50937394a80c2064ba4e9a3
kevin10v <vokakevin@icloud.com>
2025-07-28 09:25:59 +0000

    update

---
+++ hash_tables/hash_tables.h
@@ -0,0 +1,34 @@
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+#include <stddef.h>
+
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string (unique)
+ * @value: The value associated with the key
+ * @next: Points to the next node in case of collision
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table structure
+ *
+ * @size: Size of the array
+ * @array: Array of pointers to linked list (buckets)
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+
+#endif /* HASH_TABLES_H */
